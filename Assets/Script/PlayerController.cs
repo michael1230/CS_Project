2e255ff6d46b7f91f44298c271977d7b636854cb
michael1230 <zak1230@gmail.com>
2018-12-25 14:15:12 +0200

    Player and camera
    movment animations for player
    and camera on player as prefab
    new art for player
    new animation for player(rain)

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,16 +6,16 @@ public class PlayerController : MonoBehaviour
 {
 
 
-    public float walkSpeed;
-    public float runSpeed;
-    public Rigidbody2D theRB;
-    public Animator myAnim;
-    public static PlayerController instance;
-    public bool canMovePlayer = true;
+    public float walkSpeed;//the speed of walk
+    public float runSpeed;//the speed of run
+    public Rigidbody2D theRB;//reference the Rigidbody2D
+    public Animator myAnim;//reference the Animator
+    public static PlayerController instance;//makes only one instance of player
+    public bool canMovePlayer = true;//a flag to spot the player when needed
 
-    public string areaTransitionName;
-    private Vector3 bottomLeftLimit;
-    private Vector3 topRightLimit;
+    public string areaTransitionName;//the name to next area
+    private Vector3 bottomLeftLimit;//the first limit of the  map
+    private Vector3 topRightLimit;//the second limit of the  map
 
     // Use this for initialization
     void Start ()
@@ -41,16 +41,49 @@ public class PlayerController : MonoBehaviour
     {
         if (canMovePlayer)
         {
-            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * walkSpeed;
-
+            if ( (Input.GetButton("Horizontal")|| Input.GetButton("Vertical"))&&(Input.GetKey(KeyCode.LeftShift)))//if arrows and shift are pressed
+            {
+                myAnim.SetBool("isRunning", true);
+                myAnim.SetBool("isWalking", true);
+                theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * runSpeed;
+            }
+            else if((Input.GetButton("Horizontal") || Input.GetButton("Vertical")))//if only arrows are pressed
+            {
+                myAnim.SetBool("isRunning", false);
+                theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * walkSpeed;
+            }
+            else //if nothing is pressed
+            {
+                myAnim.SetBool("isRunning", false);
+                myAnim.SetBool("isWalking", false);
+                theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * walkSpeed;
+            }
         }
         else
         {
             theRB.velocity = Vector2.zero;
         }
+        myAnim.SetFloat("moveX", theRB.velocity.x);//Update the x velocity 
+        myAnim.SetFloat("moveY", theRB.velocity.y);//Update the y velocity 
 
-        
-
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {//keep the player in the right direction 
+            if (canMovePlayer)
+            {
+                myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+                myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+            }
+        }
+        //keep the camera inside the bounds
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
 
     }
+    public void SetBounds(Vector3 botLeft, Vector3 topRight)
+    {
+        bottomLeftLimit = botLeft + new Vector3(.5f, 1f, 0f);
+        topRightLimit = topRight + new Vector3(-.5f, -1f, 0f);
+    }
+
+
+
 }
