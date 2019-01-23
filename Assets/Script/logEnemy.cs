841959ad6b2036ff1e5cd81b7d896c3f64866c58
alexsg18 <alexsg18.ag@gmail.com>
2019-01-23 22:01:15 +0200

    Knockback
    added a script for when hitting enemy it will be knockback

--- Assets/Script/logEnemy.cs
+++ Assets/Script/logEnemy.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class logEnemy : EnemyOnMap {
 
+    public Transform target;
+    public float chaseRaidius;
+    public float attackRadius;
+    public Transform homePosition; //for returning to base if not attacking
 
 	// Use this for initialization
 	void Start () {
-		
+        target = GameObject.FindWithTag("Player").transform; //finds the player location
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        CheckDistance();
 	}
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRaidius && Vector3.Distance(target.position, transform.position) >attackRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime); //the log will move to the player
+        }
+    }
+
 }
