47d1394e677c86fba6262c503b03613af794b90e
alexsg18 <alexsg18.ag@gmail.com>
2019-01-26 20:40:50 +0200

    Revert "Merge branch 'master' of https://github.com/michael1230/CS_Project"
    This reverts commit 0697eebfc93cb3951cf7e9c86e0f34a3d5e08761, reversing
    changes made to 2cb1d774d3d7662c47ea82b169707967c53a34e6.

--- Assets/Script/logEnemy.cs
+++ Assets/Script/logEnemy.cs
@@ -9,13 +9,13 @@ public class logEnemy : EnemyOnMap {
     public float chaseRaidius;
     public float attackRadius;
     public Transform homePosition; //for returning to base if not attacking
-    public Animator anim;
+   // public Animator anim;
 
 	// Use this for initialization
 	void Start () {
         currentState = EnemyState.idle;
         myRigidbody = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+     //   anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform; //finds the player location
 	}
 	
@@ -31,46 +31,9 @@ public class logEnemy : EnemyOnMap {
             if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
             {
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime); //the log will move to the player
-                changeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
-                anim.SetBool("wakeUp", true);
             }
-        }
-        else if(Vector3.Distance(target.position, transform.position) > chaseRaidius)
-        {
-            anim.SetBool("wakeUp", false);
-        }
-    }
-    private void SetAnimFloat(Vector2 setVector)
-    {
-        anim.SetFloat("moveX", setVector.x);
-        anim.SetFloat("moveY", setVector.y);
-    }
-
-    private void changeAnim(Vector2 direction)
-    {
-        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-            {
-            if (direction.x > 0)
-            {
-                SetAnimFloat(Vector2.right);
-            }
-            else if (direction.x < 0)
-            {
-                SetAnimFloat(Vector2.left);
-            }
-        }else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
-        {
-            if (direction.y > 0)
-            {
-                SetAnimFloat(Vector2.up);
-            }
-            else if (direction.y < 0)
-            {
-                SetAnimFloat(Vector2.down);
-            }
-
         }
     }
 
