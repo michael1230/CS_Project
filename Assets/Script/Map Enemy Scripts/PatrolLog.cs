64a696a9a94f60f994775f9032a8dbb37063bc07
alexsg18 <alexsg18.ag@gmail.com>
2019-05-17 11:43:25 +0300

    small changes

--- Assets/Script/Map Enemy Scripts/PatrolLog.cs
+++ Assets/Script/Map Enemy Scripts/PatrolLog.cs
@@ -22,7 +22,7 @@ public class PatrolLog : logEnemy {
   //  WorldTile thisNode;
 
 
-    IEnumerator Moveback()
+    IEnumerator Moveback()//NEED TO PUT THE DOT IN THE MIDDLE AND ROUND IT 4.485 == 4.5
     {
        // WorldTile thisNode = grid.NodeFromPosition(transform.position);
        // WorldTile dotNode = grid.NodeFromPosition(AstarPoint.position);
