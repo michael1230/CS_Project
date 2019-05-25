e0c52cd03ee76cf59e1de4a0979a743cc6dd4c13
michael1230 <zak1230@gmail.com>
2019-05-25 21:25:13 +0300

    ChronoDesert4

---
+++ Assets/Script/MapChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChange : MonoBehaviour
+{
+
+    public AreaExit toMaps;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GameManager.instance.numberOfElement==0)
+        {
+            toMaps.areaToLoad = "";
+            toMaps.areaTransitionName = "";
+        }
+        else if (GameManager.instance.numberOfElement == 1)
+        {
+
+        }
+        else if (GameManager.instance.numberOfElement == 2)
+        {
+
+        }
+        else if (GameManager.instance.numberOfElement == 3)
+        {
+
+        }
+    }
+}
